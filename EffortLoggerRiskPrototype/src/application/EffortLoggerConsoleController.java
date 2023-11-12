8029615cca5bba800a96ec5bf443449c38adbaaf
LucasKeylon <98484473+LucasKeylon@users.noreply.github.com>
2023-11-11 18:08:27 -0700

    Update EffortLoggerConsoleController.java

--- EffortLoggerRiskPrototype/src/application/EffortLoggerConsoleController.java
+++ EffortLoggerRiskPrototype/src/application/EffortLoggerConsoleController.java
@@ -56,4 +56,12 @@ public class EffortLoggerConsoleController implements Initializable{
 		stage.setScene(scene);;
 		stage.show();
 	}
+
+	public void switchToLogs(ActionEvent event) throws IOException{
+		root = FXMLLoader.load(getClass().getResource("logs.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);;
+		stage.show();
+	}
 }
